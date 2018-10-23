47083882b61740523c79fd6e52ca871b6dd3fa50
alumno Secundaria <al3201@cedi.local>
2018-10-23 14:07:37 -0500

    Se creó una branch para el album 2 y nuevas paginas

---
+++ src/pages/artista2/artista2.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { Artista2Page } from './artista2';
+
+@NgModule({
+  declarations: [
+    Artista2Page,
+  ],
+  imports: [
+    IonicPageModule.forChild(Artista2Page),
+  ],
+})
+export class Artista2PageModule {}
